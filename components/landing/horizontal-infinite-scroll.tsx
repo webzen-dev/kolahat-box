bc53b8a150a34d218bbbde69684c1f892b1588ef
webzen <webzen.ir@gmail.com>
2026-02-11 22:41:05 +0330

    refactor(main): lift data to main page and pass via props

--- components/landing/horizontal-infinite-scroll.tsx
+++ components/landing/horizontal-infinite-scroll.tsx
@@ -1,31 +1,27 @@
-import clsx from 'clsx';
-import Image from 'next/image';
+import clsx from "clsx";
+import Image from "next/image";
 
-export default function HorizontalInfiniteScroll() {
-  const Images = [
-    '/images/brands/1f1acb614ef91eb3832b69eb918f0208aa7480ca.png',
-    '/images/brands/290c29b78f613ee1f9efc09d08393c964757f0bb.png',
-    '/images/brands/88b3c87c55ef5753d9a78e45aef4d219c86a8843.png',
-    '/images/brands/91be6524233631974c49e17393a43726e9680864.png',
-    '/images/brands/afc7cbd15be18beb1d44d0845b09bf3cff953c7b.png',
-  ];
-
+export default function HorizontalInfiniteScroll({
+  images,
+}: {
+  images: string[];
+}) {
   return (
     <div
       className={clsx(
-        'container mx-auto flex flex-nowrap items-center overflow-hidden',
-        'select-none py-5 horizontal-infinite-scroll-container group'
+        "flex flex-nowrap items-center overflow-hidden",
+        "select-none mb-30 horizontal-infinite-scroll-container group",
       )}
     >
       {[0, 1].map((_, i) => (
         <div
           key={i}
           className={clsx(
-            'flex items-center justify-around min-w-full',
-            'gap-4 pe-4 horizontal-infinite-scroll'
+            "flex items-center justify-around min-w-full",
+            "gap-4 pe-4 horizontal-infinite-scroll",
           )}
         >
-          {Images.map((img, i) => (
+          {images.map((img, i) => (
             <Image
               src={img}
               key={i}
