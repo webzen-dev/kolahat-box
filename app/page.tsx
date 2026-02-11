d01c4a122be54fc7a765b37e95b7671f40222f26
webzen <webzen.ir@gmail.com>
2026-02-11 21:27:07 +0330

    feat(horizontal-infinite-scroll): Adding horizontal-infinite-scroll components

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import BrandStorySection from "@/components/landing/brand-story-section";
-import BrandsSection from "@/components/landing/brands-section";
+import HorizontalInfiniteScroll from "@/components/landing/horizontal-infinite-scroll";
+import BrandsSection from "@/components/landing/horizontal-infinite-scroll";
 import HomeHeroSection from "@/components/landing/home-hero-section";
 
 export default function Home() {
   return (
     <section className="flex flex-col">
-      <HomeHeroSection/>
-      <BrandStorySection/>
-      <BrandsSection/>
+      <HorizontalInfiniteScroll/>
     </section>
   );
 }
