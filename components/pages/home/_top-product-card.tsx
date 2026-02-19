37b07f3f1e21ca74e66f661aa421d1f0b777f1a2
webzen <webzen.ir@gmail.com>
2026-02-19 16:25:57 +0330

    زز

--- components/pages/home/_top-product-card.tsx
+++ components/pages/home/_top-product-card.tsx
@@ -67,7 +67,7 @@ export default function TopProductCard({ product }: { product: Product }) {
             <BuildingStorefrontIcon
               className={clsx(
                 "h-15 w-15 p-5 rounded-full",
-                "bg-surface-quaternary text-background",
+                "bg-surface text-background",
               )}
             />
           </div>
