4052d5b717c8c9d9e1f4db3bd1d4003eee6671dc
webzen <webzen.ir@gmail.com>
2026-02-21 17:06:02 +0330

    ddddd

--- components/pages/home/brand-story-section.tsx
+++ components/pages/home/brand-story-section.tsx
@@ -48,7 +48,6 @@ export default function BrandStorySection({
           "grid-cols-1 md:grid-cols-3 lg:grid-cols-4",
         )}
       >
-{/*  */}
         {columns.map((col, colIndex) => (
           <div
             key={colIndex}
